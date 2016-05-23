038e216f5001b89400595275bc5487819cea8dbf
Hawken Rives <hawkrives@gmail.com>
2016-05-22 19:52:40 -0500

    make flatten iterate over tuples

--- syllogizmos/contradictions.py
+++ syllogizmos/contradictions.py
@@ -9,7 +9,7 @@ def flatten(*args):
     # from http://stackoverflow.com/a/2158532/2347774
     for l in args:
         for el in l:
-            if isinstance(el, list) and not isinstance(el, str):
+            if (isinstance(el, list) or isinstance(el, tuple)) and not isinstance(el, str):
                 yield from flatten(el)
             else:
                 yield el
@@ -17,6 +17,6 @@ def flatten(*args):
 
 def buildContradictions(truths):
     '''Creates a list of contradictory words from statements in the dictonary'''
-    terms = flatten([[k, v] for k, v in truths.items()])
+    terms = flatten(truths.items())
     unique = set(terms)
     print(unique)
